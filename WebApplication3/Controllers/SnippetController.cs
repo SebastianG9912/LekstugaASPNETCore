f0673e05cc115e7fd9a864b7fb21877996335157
Sebastian Gustafsson <sebastian.gustafsson@utb.ecutbildning.se>
2022-03-07 16:01:05 +0100

    Add project files.

---
+++ WebApplication3/Controllers/SnippetController.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyASPNETCore.Controllers
+{
+    public class Snippet : Controller
+    {
+        /**
+         * Kallas för "Action Metod".
+         * Är en endpoint.
+         */
+        public IActionResult Index()
+        {
+            return Content("Test");
+        }
+
+        /**
+         * Kallas för "Action Metod".
+         * Är en endpoint.
+         */
+        public string Welcome(string name)
+        {
+            return $"Hej och Välkommen {name}!";
+        }
+
+        /**
+         * Skapa en action metod "Flip" som använder Random klassen
+         * för att antingen returnera "heads" eller "tails".
+         */
+        public string Flip()
+        {
+            Random random = new Random();
+
+            int randomNmbr = random.Next(0, 2);
+            if (randomNmbr > 0)
+            {
+                return "Heads";
+            }
+
+            return "Tails";
+        }
+
+        /**
+         * Skapa en action metod "Missing" som
+         * använder NotFound(), vad händer när
+         * rutten besöks?
+         */
+        public IActionResult Missing()
+        {
+            return NotFound("404");
+        }
+
+        /**
+         * Skapa en action metod "Search" som använder Redirect()
+         * och en queryparameter för att skicka besökaren till
+         * https://www.google.se?q=[sökning].
+         * (Det finns en Uri klass i .NET motsvarande URL i
+         * javascript)
+         */
+        public IActionResult Search(string searchParam)
+        {
+            return Redirect("https://www.google.se?q=" + searchParam);
+        }
+
+        /**
+         * Skapa en action metod "Person" som använder Json()
+         * och en POCO C# klass Person som du hittar på, för
+         * att skicka tillbaka en JSON text.
+         */
+        public IActionResult Person()
+        {
+            var person = new Person
+            {
+                Name = "Basse",
+                Age = 22
+            };
+
+            return Json(person);
+        }
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ WebApplication3/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualBasic;
+
+var builder = WebApplication.CreateBuilder(args);
+//fas 1 - konfigurering av webbapp
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+//fas 2 - middleware pipelining
+
+/*app.Use(async (context, nextMiddlewareStep) =>
+{
+    //HTTP request påväg in
+    Console.WriteLine($"Request for: {context.Request.Path}");
+
+    await nextMiddlewareStep();
+
+    //HTTP request påväg ut
+    Console.WriteLine($"Answer status code: {context.Response.StatusCode}");
+    return;
+});*/
+
+//middleware steg
+app.UseRouting();
+
+//mappning
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=snippet}/{action=index}");
+//app.MapRazorPages();
+//Behövs inte för MVC eller Razor Pages
+/*var endpoint = () => "Hemlig endpoint";
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/hemlig", () => endpoint);
+app.MapGet("/hemlig/mer", endpoint);
+app.MapGet("/namn/{name}", (string name) => $"Hej {name}!");
+app.MapGet("/welcome", () => "Hej och välkommen!");*/
+
+app.Run();
+//fas 3 - server startad
